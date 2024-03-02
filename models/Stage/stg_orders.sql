215b9600e5987814d62203a366f94fba8ccb9bcc
saurabhpandey27796@gmail.com <saurabhpandey27796@gmail.com>
2024-03-02 10:38:52 +0000

    added stage

---
+++ models/Stage/stg_orders.sql
@@ -0,0 +1,20 @@
+select 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice,
+o.ordercostprice,
+ ordersellingprice-ordercostprice as orderprofit,
+ p.productid,
+ p.category,
+ p.productname,
+ p.subcategory,
+ c.customername,
+ c.segment,
+ c.country
+from {{ ref('raw_orders') }} o
+left join {{ ref('raw_customer') }} c
+on o.customerid=c.customerid
+left join {{ ref('raw_product') }} p
+on o.productid=p.productid
